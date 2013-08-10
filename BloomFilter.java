2bab846f7d0e03dfd01f519c1bc36c74809b7f28
Ryan Drapeau <rdrapeau1@gmail.com>
2013-08-09 20:07:20 -0700

    Hashing

--- BloomFilter.java
+++ BloomFilter.java
@@ -32,10 +32,35 @@ public class BloomFilter<E> {
 		elements = new BitSet(size * BITS_PER_OBJECT);
 	}
 	
+	/**
+	 * Adds an element into the BloomFilter to remember. 
+	 * 
+	 * @param element - The element to add to the set 
+	 */
 	public void add(E element) {
-		elements.set(defaultHash(element));
-		
+		int initial = defaultHash(element);
+		int offset = secondaryHash(element);
+		for (int i = initial; i < elements.length(); i += offset) {
+			elements.set(i);
+		}
 		size++;
+	}
+	
+	/**
+	 * Returns whether or not an element is contained within the BloomFilter.
+	 * 
+	 * @param element - The element to check
+	 * @return True if the element is in the bloom filter and false otherwise
+	 */
+	public boolean contains(E element) {
+		int initial = defaultHash(element);
+		int offset = secondaryHash(element);
+		for (int i = initial; i < elements.length(); i += offset) {
+			if (!elements.get(i)) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	/**
@@ -46,5 +71,21 @@ public class BloomFilter<E> {
 	 */
 	private int defaultHash(E element) {
 		return element.hashCode() % elements.length();
+	}
+	
+	/**
+	 * Applies a supplemental hash function to a given hashCode, which
+	 * defends against poor quality hash functions.  This is critical
+	 * because HashMap uses power-of-two length hash tables, that
+	 * otherwise encounter collisions for hashCodes that do not differ
+	 * in lower bits. Note: Null keys always map to hash 0, thus index 0.
+	 * 
+	 * @param element - The element to add to the BloomFilter
+	 * @return A secondary index of the element
+	 */
+	private int secondaryHash(E element) {
+		int h = element.hashCode();
+		h ^= (h >>> 20) ^ (h >>> 12);
+		return (h ^ (h >>> 7) ^ (h >>> 4)) % elements.length();
 	}
 }
---
+++ Test.java
@@ -0,0 +1,13 @@
+package Cracking_The_Coding_Interview;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
