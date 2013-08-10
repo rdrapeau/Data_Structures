e8ed3a9f0b9b0ce1cb20ab1edfaf451365452c35
Ryan Drapeau <rdrapeau1@gmail.com>
2013-08-09 19:55:59 -0700

    Constructor

--- BloomFilter.java
+++ BloomFilter.java
@@ -1,5 +1,50 @@
 package Cracking_The_Coding_Interview;
 
-public class BloomFilter {
+import java.util.BitSet;
 
+/**
+ * Implementation of the Bloom Filter data structure (also known as the HashSet)
+ * @author RDrapeau
+ *
+ */
+public class BloomFilter<E> {
+	/**
+	 * The number of bits per object in the filter: n / |S| =  # of bits per object
+	 */
+	private static final int BITS_PER_OBJECT = 8;
+	
+	/**
+	 * The elements of the BloomFilter.
+	 */
+	private BitSet elements;
+	
+	/**
+	 * The number of elements in the BloomFilter.
+	 */
+	private int size;
+	
+	/**
+	 * Constructs a new BloomFilter with 8 bits assigned to each object.
+	 * 
+	 * @param size - The number of objects to be added to the BloomFilter
+	 */
+	public BloomFilter(int size) {
+		elements = new BitSet(size * BITS_PER_OBJECT);
+	}
+	
+	public void add(E element) {
+		elements.set(defaultHash(element));
+		
+		size++;
+	}
+	
+	/**
+	 * Returns the index of the hash code method element implements.
+	 * 
+	 * @param element - The element to add to the BloomFilter
+	 * @return The index of the element in the set
+	 */
+	private int defaultHash(E element) {
+		return element.hashCode() % elements.length();
+	}
 }
