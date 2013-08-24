d66753ab92cab6a00d631394adffeeb67b737e6e
Ryan Drapeau <rdrapeau1@gmail.com>
2013-08-23 20:35:44 -0700

    Find

--- Advanced/DisjointSet.java
+++ Advanced/DisjointSet.java
@@ -1,10 +1,54 @@
 package Advanced;
 
-public class DisjointSet {
-	private int[] size;
-	private int[] id;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DisjointSet<E> {
+	private Map<E, Node> convert;
 	
-	public DisjointSet(int n) {
+	/**
+	 * Constructs a new DisjoinSet
+	 */
+	public DisjointSet() {
+		convert = new HashMap<E, Node>();
+	}
+	
+	/**
+	 * Returns whether or not the two elements belong to the same component.
+	 * 
+	 * @param first - The first element
+	 * @param other - The second element
+	 * @return True if the two elements belong to the same component and false otherwise
+	 */
+	public boolean find(E first, E other) {
+		return root(convert.get(first)) == root(convert.get(other));
+	}
+	
+	/**
+	 * Returns the overall root of the child and compresses the tree.
+	 * 
+	 * @param child - The child Node
+	 * @return The overall root of the component
+	 */
+	private Node root(Node child) {
+		if (child != null) {
+			while (child.parent != null) {
+				child.parent = child.parent.parent;
+				child = child.parent;
+			}
+		}
+		return child;
+	}
+	
+	private class Node {
+		private Node parent;
+		private E element;
+		private int rank;
 		
+		public Node(E element, int rank, Node parent) {
+			this.element = element;
+			this.rank = rank;
+			this.parent = parent;
+		}
 	}
 }
