b52e25c369f0e1f115be59922561ff0ec3615d37
Ryan Drapeau <rdrapeau1@gmail.com>
2013-08-28 19:52:42 -0700

    Bin Packing

--- Algorithms/BinProblem.java
+++ Algorithms/BinProblem.java
@@ -15,8 +15,8 @@ import Support.Bucket;
  */
 public class BinProblem {
 	public static void main(String[] args) {
-		int[] jobs = {1, 2, 6, 7, 8, 9, 10};
-		System.out.println(distribute(jobs, 2));
+		int[] jobs = {1, 1, 2, 3, 5, 4, 2, 3, 2, 1};
+		System.out.println(packBuckets(jobs, 11));
 	}
 
 	/**
@@ -39,6 +39,36 @@ public class BinProblem {
 			Bucket b = q.remove();
 			b.add(jobs[i]);
 			q.add(b);
+		}
+		return buckets;
+	}
+	
+	/**
+	 * Packs items into bins of capacity - creating new ones when needed.
+	 * 
+	 * @param items - Items to pack
+	 * @param capacity - Capacity of each bin
+	 * @return A list of buckets
+	 */
+	public static List<Bucket> packBuckets(int[] items, int capacity) {
+		Arrays.sort(items);
+		List<Bucket> buckets = new ArrayList<Bucket>();
+		for (int i = items.length - 1; i >= 0; i--) { // O(n * k)
+			boolean done = items[i] > capacity ? true : false;
+			int index = 0;
+			Bucket b;
+			while (!done && index < buckets.size()) {
+				b = buckets.get(index++);
+				if (b.sum() + items[i] <= capacity) {
+					b.add(items[i]);
+					done = true;
+				}
+			}
+			if (!done) { // Create a new Bucket
+				b = new Bucket();
+				b.add(items[i]);
+				buckets.add(b);
+			}
 		}
 		return buckets;
 	}
