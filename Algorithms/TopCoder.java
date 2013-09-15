b644b014c7fe3a97286f6f33535482f20baf92ca
Ryan Drapeau <rdrapeau1@gmail.com>
2013-09-15 12:29:20 -0700

    Same Birthday Problem

--- Algorithms/TopCoder.java
+++ Algorithms/TopCoder.java
@@ -1,13 +1,24 @@
 package Algorithms;
 
 public class TopCoder {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(birthdayOdds(84, 9227));
 	}
-
+	
+	/**
+	 * Returns the number of people required to be in the room in order to guarantee that there is
+	 * at least a targetPercent chance that two or more have the same birthday.
+	 * 
+	 * @param targetPercent - Desired percentage of two or more having the same birthday
+	 * @param daysPerYear - Days in a year
+	 * @return The number of people in the room
+	 */
+	public static int birthdayOdds(int targetPercent, int daysPerYear) {
+		int count = daysPerYear;
+		double currentPercent = 100;
+		while (currentPercent > 100 - targetPercent && count > 0) {
+			currentPercent *= count-- / (double) daysPerYear;
+		}
+		return daysPerYear - count;
+	}
 }
